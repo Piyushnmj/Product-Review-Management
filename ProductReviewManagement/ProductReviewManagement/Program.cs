24f553c06392a0a7d4ada063c548acf144b6f6a5
Piyushnmj <piyushnmj@yahoo.com>
2022-12-16 13:17:20 +0530

    Product Review Class

--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -5,6 +5,31 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Management Program");
+
+            //UC1
+            List<ProductReview> objProductReviewList = new List<ProductReview>()
+            {
+                new ProductReview() {ProductID = 1, UserID= 1, Rating=5, Review="Good", isLike=true},
+                new ProductReview() {ProductID = 2, UserID= 1, Rating=4, Review="Good", isLike=true},
+                new ProductReview() {ProductID = 3, UserID= 2, Rating=4.5, Review="Good", isLike=true},
+                new ProductReview() {ProductID = 4, UserID= 2, Rating=3, Review="Nice", isLike=true},
+                new ProductReview() {ProductID = 5, UserID= 3, Rating=3.5, Review="Nice", isLike=true},
+                new ProductReview() {ProductID = 6, UserID= 4, Rating=3.5, Review="Nice", isLike=true},
+                new ProductReview() {ProductID = 7, UserID= 5, Rating=1, Review="Bad", isLike=true},
+                new ProductReview() {ProductID = 8, UserID= 4, Rating=2.5, Review="Bad", isLike=true},
+                new ProductReview() {ProductID = 9, UserID= 6, Rating=1, Review="Bad", isLike=true},
+                new ProductReview() {ProductID = 10, UserID= 3, Rating=1, Review="Bad", isLike=true},
+            };
+
+            Console.WriteLine("\n**********All Product Reviews**********\n");
+            foreach (var list in objProductReviewList)
+            {
+                Console.WriteLine($"ProductID : {list.ProductID}\n" +
+                                  $"UserID    : {list.UserID}\n" +
+                                  $"Rating    : {list.Rating}\n" +
+                                  $"Review    : {list.Review}\n" +
+                                  $"isLike    : {list.isLike}\n");
+            }
         }
     }
 }
